658150d87dfd10fddda8177073b13ca64139a918
Julianalopesco <julianalopesmf@gmail.com>
2023-04-25 22:12:27 -0300

    comandos DML no oracle

---
+++ scriptDML.sql
@@ -0,0 +1,26 @@
+REM   Script: Comandos CRUD - Turma
+REM   Inicio de comandos CRUD para um banco de dados de uma turma
+
+insert into aluno values (1, 'jose da silva', default,10);
+
+insert into aluno values (1, 'joao antonio', '10-oct-22,11');
+
+insert into aluno values (1, 'maria da sikva','10-oct-22,11');
+
+select * from aluno;
+
+update aluno 
+set nome='maria da silva' 
+where id=3;
+
+select * from aluno;
+
+where id=3;
+
+select * from aluno;
+
+delete from aluno 
+where id=3;
+
+select * from aluno;
+
